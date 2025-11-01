a9b0cb124c8d2bebf10eaac46824ad9e111c9306
sargsyanmarat1996 <sargsyanmarat1996@gmail.com>
2025-11-01 23:10:44 +0300

    Update schema_editing.sql

--- travel_agency/schema_editing.sql
+++ travel_agency/schema_editing.sql
@@ -6,3 +6,21 @@ OR DATEDIFF(bookings.booking_date, bookings.travel_date) < - 190
 
 
 --Add sex for clietns--
+UPDATE customers SET sex = 'M';
+
+UPDATE customers SET sex = 'F'
+WHERE customers.fullname LIKE 'S%'
+OR customers.fullname LIKE 'E%'
+OR customers.fullname LIKE 'V%'
+OR customers.fullname = 'Amber West'
+OR customers.fullname = 'Brenda Ramos'
+OR customers.fullname = 'Cynthia Mills MD'
+OR customers.fullname = 'Diane Schultz'
+OR customers.fullname = 'Elizabeth Woods'
+OR customers.fullname = 'Diane Schultz'
+OR customers.fullname = 'Katrina Perry'
+OR customers.fullname = 'Nicole Thomas'
+OR customers.fullname = 'Teresa Barnett'
+OR customers.fullname = 'Tracy Sandoval'
+OR customers.fullname = 'Teresa Barnett'
+OR customers.fullname = 'Teresa Barnett'
