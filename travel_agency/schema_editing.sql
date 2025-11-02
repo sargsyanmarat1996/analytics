a6f9c7af0bc2e411d308dc0be73c59b85fa310c7
sargsyanmarat1996 <sargsyanmarat1996@gmail.com>
2025-11-02 09:59:37 +0300

    Update schema_editing.sql

--- travel_agency/schema_editing.sql
+++ travel_agency/schema_editing.sql
@@ -1,4 +1,4 @@
---Set difference between booking_date and travel_date within range 1 and 150 days--
+--Set difference between booking_date and travel_date within the range 1 and 150 days--
 UPDATE bookings
 SET bookings.travel_date = DATE_ADD(bookings.booking_date, INTERVAL FLOOR(1 + RAND()*(100-1)) DAY)
 WHERE DATEDIFF(bookings.booking_date, bookings.travel_date) > 190 
