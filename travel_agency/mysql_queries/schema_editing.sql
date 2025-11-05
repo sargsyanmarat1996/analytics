a07c577cb421c02f1fcc5023ecad11b066816b53
sargsyanmarat1996 <sargsyanmarat1996@gmail.com>
2025-11-05 21:40:13 +0300

    Update schema_editing.sql

--- travel_agency/mysql_queries/schema_editing.sql
+++ travel_agency/mysql_queries/schema_editing.sql
@@ -9,6 +9,7 @@ SET bookings.booking_date = DATE_ADD(bookings.travel_date, INTERVAL FLOOR(1 + RA
 WHERE DATEDIFF(bookings.booking_date, bookings.travel_date) > 1
 OR DATEDIFF(bookings.booking_date, bookings.travel_date) < - 1  
 
+  
 --Add sex for clietns--
 
 ALTER TABLE customers ADD sex CHAR(1). 
@@ -31,3 +32,31 @@ OR customers.fullname = 'Teresa Barnett'
 OR customers.fullname = 'Tracy Sandoval'
 OR customers.fullname = 'Teresa Barnett'
 OR customers.fullname = 'Teresa Barnett'
+
+
+--Set difference between dividing budget on day mumber — 100 and 500--
+
+UPDATE bookings
+JOIN itineraries ON(bookings.booking_id = itineraries.booking_id)
+SET bookings.budget = FLOOR(100 + RAND()*(500-1))
+WHERE itineraries.day_number = 1;
+
+UPDATE bookings
+JOIN itineraries ON(bookings.booking_id = itineraries.booking_id)
+SET bookings.budget = FLOOR(200 + RAND()*(1000-1))
+WHERE itineraries.day_number = 2;
+
+UPDATE bookings
+JOIN itineraries ON(bookings.booking_id = itineraries.booking_id)
+SET bookings.budget = FLOOR(300 + RAND()*(1500-1))
+WHERE itineraries.day_number = 3;
+
+UPDATE bookings
+JOIN itineraries ON(bookings.booking_id = itineraries.booking_id)
+SET bookings.budget = FLOOR(400 + RAND()*(2000-1))
+WHERE itineraries.day_number = 4;
+
+UPDATE bookings
+JOIN itineraries ON(bookings.booking_id = itineraries.booking_id)
+SET bookings.budget = FLOOR(500 + RAND()*(2500-1))
+WHERE itineraries.day_number = 5
